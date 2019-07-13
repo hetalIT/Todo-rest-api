04ceab3013ca9b77ba55c71c08e02e8c88351349
hetal <hetal93hasmukh@gmail.com>
2019-07-13 11:23:13 +0530

    add test for GET /todos/:id

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -1,5 +1,6 @@
 const expect=require("expect");
 const supertest=require("supertest");
+const {ObjectId}=require("mongodb");
 
 const {app}=require("./../server.js");
 const {Todo}=require("./../models/todo");
@@ -7,6 +8,12 @@ const {Todo}=require("./../models/todo");
 // beforeEach(done=>{
 //     Todo.remove({}).then(()=>{done();});
 // });
+const todos=[{
+    _id:new ObjectId(),text:"first step todo"
+},{
+    _id:new ObjectId(),text:"second step todo"
+}];
+//Todo.insertMany(todos);
 describe("POST /Todos",()=>{
     it("should a new todo work",done=>{
         var text="coding java";
@@ -57,5 +64,38 @@ describe("GET /todos",()=>{
             //console.log(JSON.stringify(res.body,undefined,2));
             done();
         });
+    });
+});
+
+describe("GET /todos/:id",()=>{
+    it("should return specific todo item",done=>{
+        supertest(app)
+        .get(`/todos/${todos[0]._id.toHexString()}`)
+        .expect(200)
+        .expect((res)=>{
+            //console.log(res.body);
+            expect(res.body.t.text).toBe(todos[0].text);
+        })
+        .end(done);
+    });
+
+    it("should return 404 if todo not found",done=>{
+        supertest(app)
+        .get(`/todos/${todos[0]._id.toHexString()}`)
+        .expect(404)
+        .expect(res=>{ //this is not necessary as expected is 404
+            expect(Object.keys(res.body).length).toBe(0);
+        })
+        .end(done);
+    });
+
+    it("sould return 404 for non-object id",done=>{
+        supertest(app)
+        .get("/todos/123")
+        .expect(404)
+        .expect(res=>{ //this is not necessary as expected is 404
+            expect(Object.keys(res.body).length).toBe(0);
+        })
+        .end(done);
     });
 });
